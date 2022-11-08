c3c72191dfa4c78d8a17f6562f6952a81b1dd256
Avijit-Sarkar-Avi <avijit215054@gmail.com>
2022-11-08 15:42:32 +0600

    first commit added blog, header, footer

---
+++ src/Pages/Blog/Blog.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Header from '../Shared/Header/Header';
+
+const Blog = () => {
+    return (
+
+        <div>
+            <Header></Header>
+
+            <div className='mb-10'>
+                <h1 className='text-2xl text-red-600'>1. Write difference between SQL and NoSQL?</h1>
+                <p>SQL is the programming language used to interface with relational databases. Relational databases model data as records in rows and tables with logical links between them. NoSQL is a class of DBMs that are non-relational and generally do not use SQL.</p>
+            </div>
+
+            <div className='mb-10'>
+                <h1 className='text-2xl text-red-600'>2. What is JWT and how does it work?</h1>
+                <p>JSON Web Token (JWT) is an open standard (RFC 7519) for securely transmitting information between parties as JSON object. It is compact, readable and digitally signed using a private key/ or a public key pair by the Identity Provider(IdP). So the integrity and authenticity of the token can be verified by other parties involved.</p>
+                <p>Basically the identity provider(IdP) generates a JWT certifying user identity and Resource server decodes and verifies the authenticity of the token using secret salt / public key. User sign-in using username and password or google/facebook. Then
+                    authentication server verifies the credentials and issues a jwt signed using either a secret salt or a private key. After that User's Client uses the JWT to access protected resources by passing the JWT in HTTP Authorization header.
+                    Resource server then verifies the authenticity of the token using the secret salt/ public key.</p>
+            </div>
+            <div className='mb-10'>
+                <h1 className='text-2xl text-red-600'>3. What is the difference between Javascript and NodeJS?</h1>
+                <p>JavaScript is a simple programming language that can be used with any browser that has the JavaScript Engine installed. Node.js, on the other hand, is an interpreter or execution environment for the JavaScript programming language. It requires libraries that can be conveniently accessed from JavaScript programming to be more helpful.</p>
+            </div>
+
+            <div className='mb-10'>
+                <h1 className='text-2xl text-red-600'>4. How does NodeJS handle multiple requests at the same time?</h1>
+                <p>NodeJS receives multiple client requests and places them into EventQueue. NodeJS is built with the concept of event-driven architecture. NodeJS has its own EventLoop which is an infinite loop that receives requests and processes them.</p>
+            </div>
+        </div>
+    );
+};
+
+export default Blog;
---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDsNLaG-RA9e7cAd0qpcnJWQ8cGlBzaaQQ",
+    authDomain: "assignment-11-cdfe0.firebaseapp.com",
+    projectId: "assignment-11-cdfe0",
+    storageBucket: "assignment-11-cdfe0.appspot.com",
+    messagingSenderId: "327739334219",
+    appId: "1:327739334219:web:cb0529396561de7bd737ce"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
